6de59d775d32a668223d26cb014b0e8b36a450cc
Parkgeum <37042238+Parkgeum@users.noreply.github.com>
2018-09-11 12:50:36 +0900

    Revert "변경내역"
    This reverts commit ac3fc3e71a11dd8ebe31e95dd237e11a90bff929.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,11 @@ public class Main {
 	// write your code here
         int year = 2017;
         System.out.println("Welcom to "+year+" Java Class");
+<<<<<<< HEAD
         System.out.println("version2");
         System.out.println("Welcome Web&App");
 
+=======
+>>>>>>> parent of ac3fc3e... 변경내역
     }
 }
