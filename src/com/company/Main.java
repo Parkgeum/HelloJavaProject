40066ee77f13dc552e22c79f44fab8095483bc50
Parkgeum <37042238+Parkgeum@users.noreply.github.com>
2018-09-11 12:39:27 +0900

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int year = 2017;
+        System.out.println("Welcom to "+year+" Java Class");
+    }
+}
